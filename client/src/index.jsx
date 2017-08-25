fb75dfdc0a00c1ea5c08fba76e5019b511213256
David Buchan-Swanson <david.buchanswanson@gmail.com>
2017-08-26 01:25:39 +1000

    fix fb load order race condition

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import './util/fb-loader';
 
 const r = (Component) => {
   render(<Component />, document.getElementById('app'))
