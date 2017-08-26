40e2f7994d12f884374c7c325deca3b7bd629d23
Khuslen Baatarsukh <khuslen96@yahoo.com>
2017-08-26 21:17:12 +1000

    open and close trips

---
+++ client/src/components/TripDetails.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Button, Header, Image } from 'semantic-ui-react';
+
+export class TripDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getTripDetails = this.getTripDetails.bind(this);
+    this.printHostName = this.printHostName.bind(this);
+    //this.renderPassengerCard = this.renderPassengerCard.bind(this);
+    
+    this.onClick = e => {
+      e.target.value = props.id;
+      props.onClick(e);
+    }
+  }
+
+  getTrips() {
+    return (
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+      >
+        <Button style={{ marginTop: 3, marginBottom: 3 }}>{this.props.id}</Button>
+      </div>
+    )
+  }
+
+  printHostName() {
+    return (
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        >
+          {this.props.hostedBy}
+        </div>
+    )
+  }
+
+  renderPassengerCard(passenger) {
+    return (
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        >
+          {passenger.id}
+          {/* {passenger.accepted} */}
+          {<Image 
+            shape="circular"
+            src={passenger.photo}
+            size="tiny"
+            style={{ marginBottom: 10 }}
+          />}
+        </div>
+    )
+  }
+
+  getTripDetails() {
+      return (
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        > Trip to LOCATION <br></br>
+          Leaving at {this.props.departAt} <br></br>
+          Hosted by {this.props.type === 'journey' ? this.printHostName() : "you"}
+          <br></br>Transport: {this.props.transportMethod} <br></br>
+          {/* Accepted: {this.props.passenger.map(p => p.accepted).join('. ')} */}
+          {/* Pending: {this.props.passenger.map(p =>)}} */}
+          {this.props.passengers.map(p => this.renderPassengerCard(p))}
+        </div>
+      )
+  }
+
+  render() {
+    return (
+      <div 
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+        onClick={this.onClick}
+      >
+        {this.getTrips()}
+        {this.props.open ? this.getTripDetails() : null}
+      </div>
+    )
+  }
+}
+
+export default connect()(TripDetails);
--- client/src/components/TripList.jsx
+++ client/src/components/TripList.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import compact from 'lodash/compact';
-
-const TripCard = ({ passengers }) => <div>{passengers.map(p => p.firstName).join(", ")}</div>;
+import map from 'lodash/map';
+import TripDetails from './TripDetails';
 
 export class TripList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {openTripId: null}
+    this.tripClick = e => this.setState({openTripId: e.target.value});
+  }
+
   renderTripCard(trip) {
     const passengers = compact(trip.passengers.map(p => this.props.users[p.id]));
 
@@ -19,13 +26,15 @@ export class TripList extends Component {
       tripProps = { type: 'trip' };
     }
 
-
     return (
-      <TripCard
+      <TripDetails
         key={trip.id}
         id={trip.id}
         departAt={trip.departAt}
         passengers={passengers}
+        transportMethod={trip.transportMethod}
+        open={trip.id === this.state.openTripId}
+        onClick={this.tripClick}
         {...tripProps}
       />
     );
@@ -41,9 +50,12 @@ export class TripList extends Component {
 }
 
 const mapStateToProps = (state) => {
-  const trips = Object.entries(state.trips).map(([id, trip]) => ({ id, ...trip }));
-  const journeys = Object.entries(state.journeys).map(([id, journey]) => ({ id, ...journey }));
+  console.log(state.trips)
+  const trips = map(state.trips, (trip, id) => ({ id, ...trip }));
+  const journeys = map(state.journeys, (journey, id) => ({ id, ...journey }));
   const allTrips = trips.concat(journeys).sort((a, b) => b.departAt - a.departAt);
+
+console.log(allTrips)
 
   return {
     trips: allTrips,
