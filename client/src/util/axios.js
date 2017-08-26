db77c3b197ac890e9e2b5b40973bca635b083b12
Dave Allie <dave@daveallie.com>
2017-08-26 10:39:50 +1000

    use localhost base url for axios in dev

--- client/src/util/axios.js
+++ client/src/util/axios.js
@@ -1,3 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = "https://ghettopool.party/api"
+axios.defaults.baseURL = process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api"
+    : "https://ghettopool.party/api"
