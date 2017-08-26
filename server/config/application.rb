f2daec48de2cde8e86405807ca5acba32996be73
Dave Allie <dave@daveallie.com>
2017-08-26 15:18:58 +1000

    Set time zone to brisbane

--- server/config/application.rb
+++ server/config/application.rb
@@ -34,6 +34,7 @@ module GhettoPool
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.time_zone = 'Brisbane'
 
     config.logger = ::Logger.new(Rails.root.join('log', "#{Rails.env}.log"))
   end
